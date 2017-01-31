a59c3de425ecb79e023e3797b33b1a95e4982ab4
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-01-31 14:59:06 +0100

    Add third project

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var a = prompt('Podaj bok a: ');
+var h = prompt('Podaj wysokość: ');
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a:' + a + ' i wysokości h:' + h + ' wynosi: ' + triangleArea);
